9d46972e6ce58168178e27198aa0ea5ab3899b4d
RostislavStarikov <146059223+RostislavStarikov@users.noreply.github.com>
2023-12-10 14:03:37 +0300

    Изменил названия функций и переменных (в camelCase), сделал нормальную обработку исключительных ситуаций, добавил выводы в main.

--- laboratory-task-4/main.cpp
+++ laboratory-task-4/main.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
 
 
-using std::cin;
-using std::cout;
-
-
-void chekN(double& n)
+void checkSize(double& size)
 {
-	try
+	std::cout << "Enter the odd size of the square matrix: ";
+	std::cin >> size;
+	if ( size != static_cast<int32_t>(size) || static_cast<int32_t>(size) % 2 == 0 || 3>size || size>101)
 	{
-		cout << "Enter the odd size of the square matrix: ";
-		cin >> n;
-		if ( n != static_cast<int32_t>(n) || static_cast<int32_t>(n) % 2 == 0 || 3>n || n>101)
-		{
-			throw n;
-		}
+		throw "Incorrect size";
 	}
-	catch (double n)
-	{		
-
-		cout << "Uncorrect size!!!\n";
-		exit(-1);
-	}
-	static_cast<int32_t>(n);
+	static_cast<int32_t>(size);
 }
 
-void create_matrix(int32_t** arr, uint32_t rows, uint32_t cols)
+void createMatrix(int32_t** arr, uint32_t rows, uint32_t cols)
 {
 	for (int i = 0; i < rows; ++i)
 	{
@@ -34,98 +21,78 @@ void create_matrix(int32_t** arr, uint32_t rows, uint32_t cols)
 }
 
 //Выбор способа заполнения матрицы
-bool MatrixFillType(char& way)
+bool matrixFillType(char& way)
 {
 	switch (way)
 	{
 	case '1':
-		return false;
-		break;
-	case '2':
 		return true;
 		break;
+	case '2':
+		return false;
+		break;
 	default:
-		setlocale(LC_ALL, "Rus");
-		cout << "Select how to fill the array: \n\t\t\t\t    1.Entering elements randomly from a range[a,b]\n\t\t\t\t    2.Entering items from the keyboard\n";
-		cin >> way;
-		return  MatrixFillType(way);
+		throw "Incorrect type of fill array!!!";
 	}
 }
 
 //Filling out the matrix
-void fillMatrix(int32_t** arr, uint32_t rows, uint32_t cols, char way) {
-	int32_t a, b;
-	if (MatrixFillType(way)) {							//filling the matrix from the keyboard
-		cout << "\n" << "Enter integer elements of matrix \n";
-		for (int i = 0; i < rows; ++i)
-		{
-			for (int j = 0; j < cols; ++j)
-			{
-				try
-				{
-					double temp = 0;
-					cin >> temp;
-					if (temp != static_cast<int32_t>(temp))
-					{
-						throw (temp);
-					}
-					arr[i][j] = static_cast<int32_t>(temp);
-				}
-				catch (double temp)
-				{
-					cout << "Invalid value!!!";
-					exit(-1);
-				}
-			}
-		}			
+void fillRand(int32_t** arr, uint32_t rows, uint32_t cols, char way)
+{
+	int32_t leftBorder, rightBorder;
+	std::cout << "Enter range [leftBorder,rightBorder] : \nLeft border = ";
+	std::cin >> leftBorder;
+	std::cout << "\nRight border = ";
+	std::cin >> rightBorder;
+	if (leftBorder != static_cast<int32_t>(leftBorder) || rightBorder != static_cast<int32_t>(rightBorder) || leftBorder == rightBorder)
+	{
+		throw "Incorrect border!!";
+		exit(-1);
 	}
-	else {
-		cout << "Enter range [a,b] : \n a = ";
-		cin >> a;
-		if (a!=static_cast<int32_t>(a))
+	if (rightBorder < leftBorder)
+	{
+		std::swap(leftBorder, rightBorder);
+	}
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
 		{
-			cout << "Invalid value!!!";
-			exit(-1);
-		}
-		cout << "\n b = ";
-		cin >> b;
-		if (a!=static_cast<int32_t>(b))
-		{
-			cout << "Invalid value!!!";
-			exit(-1);
-		}
-		if (a == b)
-		{
-			cout << "\n Invalid value!!!\n";
-			fillMatrix(arr, rows, cols, way);
-		}
-		if (b < a)
-		{
-			std::swap(a, b);
-		}
-		for (int i = 0; i < rows; ++i)
-		{
-			for (int j = 0; j < cols; ++j)
-			{
-				arr[i][j] = rand()%(b-a+1)+a;
-			}
+			arr[i][j] = rand()%(rightBorder-leftBorder+1)+leftBorder;
 		}
 	}
 }
 
-void outputMatrix(int32_t** arr, uint32_t rows, uint32_t cols)
+void fillFromKeyboard(int32_t** arr, uint32_t rows, uint32_t cols, char way)
+{
+	std::cout << "\n" << "Enter integer elements of matrix \n";
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			double temp = 0;
+			std::cin >> temp;
+			if (temp != static_cast<int32_t>(temp))
+			{
+				throw "Your elment in matrix must be integer";
+			}
+			arr[i][j] = static_cast<int32_t>(temp);
+		}
+	}			
+}
+
+void printMatrix(int32_t** arr, uint32_t rows, uint32_t cols)
 {
 	for (int i = 0; i < rows; ++i)
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			cout << arr[i][j] << '\t';
+			std::cout << arr[i][j] << '\t';
 		}
-		cout << '\n'; 
+		std::cout << '\n'; 
 	}
 }
 
-void chekNegativCols(int32_t** arr, uint32_t rows, uint32_t cols)
+bool chekNegativCols(int32_t** arr, uint32_t rows, uint32_t cols, uint32_t &collWithNegativeElements)
 {
 	int32_t j;
 	bool flag;
@@ -139,40 +106,41 @@ void chekNegativCols(int32_t** arr, uint32_t rows, uint32_t cols)
 				flag = false;
 				break; 
 			}
-			if (flag == true)
-			break;
+		}
+		if (flag)
+		{
+		collWithNegativeElements = j;
+		return true;
 		}
 	}
-
-	if (flag == true)
-	cout << "The number of the first column that does not contain negative elements:" << j+1 << "\n\n";
-	else
-		cout << "Each column contains a negative element\n\n";
+	return false;
 }
 
 
-void bypass(int32_t** arr, uint32_t rows, uint32_t cols)
+void spiralTraversalOfTheMatrix(int32_t** arr, uint32_t rows, uint32_t cols)
 {
- 	uint32_t shift = 0;	//объявляем переменную, в которой хранится величина шага при обходе по спирали uint32_t i = rows / 2, j = cols / 2;//устанавливаем координаты центрального элемента матрицы
+	uint32_t shift = 0; 			//объявляем переменную, в которой хранится величина шага при обходе по спирали 
+	uint32_t i = rows / 2, j = cols / 2;	//устанавливаем координаты центрального элемента матрицы
  	//цикл обхода 
  	do 
 	{
-		shift++;												//увеличиваем величину шага на 1  
+		++shift;									//увеличиваем величину шага на 1  
 		for (int m = 0; m < shift; m++)							//выводим n элементов со шагом вверх
-   	 	cout << arr[i--][j] << ' ';  if (shift == rows)			//если шаг равен размерности массива
-		break;													//выход  
+   	 	std::cout << arr[i--][j] << ' ';
+		if (shift == rows)								//если шаг равен размерности массива
+		break;										//выход  
 		for (int m = 0; m < shift; m++)							//выводим n элементов со шагом влево
-   		cout << arr[i][j--] << ' ';  
-		shift++;												//опять увеличиваем шаг
+   		std::cout << arr[i][j--] << ' ';  
+		++shift;									//опять увеличиваем шаг
  		for (int m = 0; m < shift; m++)							//выводим n элементов со шагом вниз   
-		cout << arr[i++][j] << ' ';
+		std::cout << arr[i++][j] << ' ';
 		for (int m = 0; m < shift; m++)							//выводим n элементов со шагом вправо   
-		cout << arr[i][j++] << ' ';
+		std::cout << arr[i][j++] << ' ';
  	} 
-	while (shift!=rows); 										//пока шаг не станет равен размерности массива
+	while (shift != rows); 									//пока шаг не станет равен размерности массива
 }
 
-void deleteArr(int32_t** arr, uint32_t rows, uint32_t cols)
+void arrMemFree(int32_t** arr, uint32_t rows, uint32_t cols)
 {
 	for (int i = 0; i < rows; ++i)
 	{
@@ -183,23 +151,52 @@ void deleteArr(int32_t** arr, uint32_t rows, uint32_t cols)
 
 int32_t main()
 {
-	char way = 2;
+try
+	{
+		char way = 2;
 
-	double n = 0;
-	chekN(n);
+		double n = 0;
+		checkSize(n);
 
-	uint32_t rows = n;
-	uint32_t cols = n;
+		uint32_t collWithNegativeElements = 0;
+		uint32_t rows = n;
+		uint32_t cols = n;
 
-	int32_t **arr = new int32_t* [rows];//создание массива указателей на массивы	
+		int32_t **arr = new int32_t* [rows];//создание массива указателей на массивы	
 
-	create_matrix(arr, rows, cols);
-	MatrixFillType(way);
-	fillMatrix(arr, rows, cols, way);
-	outputMatrix(arr, rows, cols);
-	chekNegativCols(arr, rows, cols);
-	bypass(arr, rows, cols);
-	deleteArr(arr, rows, cols);
-	
-	return 0;
+		createMatrix(arr, rows, cols);
+		std::cout << "Select how to fill the array: \n\t\t\t\t    1.Entering elements randomly from a range[a,b]\n\t\t\t\t    2.Entering items from the keyboard\n";
+		std::cin >> way;
+
+		if (matrixFillType(way))
+		{
+			fillRand(arr, rows, cols, way);
+		}
+		else
+		{
+			fillFromKeyboard(arr, rows, cols, way);
+		}
+			
+		printMatrix(arr, rows, cols);
+			
+		if (chekNegativCols(arr, rows, cols, collWithNegativeElements))
+		{	
+			std::cout << "The number of the first column that does not contain negative elements:" << collWithNegativeElements + 1 << "\n\n";
+		}
+		else
+		{
+			std::cout << "Each column contains leftBorder negative element\n\n";
+		}
+			
+		spiralTraversalOfTheMatrix(arr, rows, cols);
+			
+		arrMemFree(arr, rows, cols);
+		
+		return 0;
+	}
+	catch (const char* &msg)
+		{
+			std::cerr << msg;
+			return -1;
+		}
 }
