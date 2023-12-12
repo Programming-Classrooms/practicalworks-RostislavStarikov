7a352c330d2b771b71304102a2a0348d95ec6673
RostislavStarikov <starikovrostislav7@gmail.com>
2023-12-12 20:53:18 +0300

    Исправил названия функций

--- laboratory-task-6/main.cpp
+++ laboratory-task-6/main.cpp
@@ -3,12 +3,7 @@
 #include <ctime>
 
 
-using std::cout;
-using std::cin;
-using std::ofstream;
-using std::ifstream;
-
-void CheckInputFile(ifstream& fin)
+void CheckInputFile(std::ifstream& fin)
 {
     if (!fin.good())
     {
@@ -25,24 +20,24 @@ void CheckInputFile(ifstream& fin)
 
 }
 
-void CheckSize(double& size)
+void CheckSize(double checksize, size_t &size)
 {
-    cout << "Enter size: ";
-    cin >> size;
-    if (size <= 0 || size != static_cast<int32_t>(size))
+    std::cout << "Enter size: ";
+    std::cin >> checksize;
+    if (checksize <= 0 || checksize != static_cast<int32_t>(checksize))
     {
         throw "Uncorrect size!!!\n";
     }
-    static_cast<int32_t>(size);
+    size = static_cast<int32_t>(checksize);
 }
 
 
 template <typename T>
 void processArray(T* arr, uint32_t size) {
     for (uint32_t i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+        std::cout << arr[i] << " ";
     }
-    cout << '\n';
+    std::cout << '\n';
 }
 
 template <typename T>
@@ -93,7 +88,7 @@ void sortFrequency(T* arr, T** matr, int32_t size, uint32_t& pos)
 
 
 template <typename T>
-void InputFromFile(ifstream &fin, T* arr, uint32_t size) 
+void InputFromFile(std::ifstream &fin, T* arr, uint32_t size) 
 {
     CheckInputFile(fin);
     for (uint32_t i = 0; i < size; i++) 
@@ -105,7 +100,7 @@ void InputFromFile(ifstream &fin, T* arr, uint32_t size)
 
 
 template <typename T>
-void OutputInFile(ofstream &fout, T* arr, T** matr, uint32_t size, uint32_t pos)
+void OutputInFile(std::ofstream &fout, T* arr, T** matr, uint32_t size, uint32_t pos)
 {
     sortFrequency(arr, matr, size, pos);
     for (int32_t i = 0; i < pos; ++i)
@@ -121,21 +116,21 @@ void OutputFromConsole(T* arr, T** matr, uint32_t size, uint32_t pos)
     sortFrequency(arr, matr, size, pos);
     for (int32_t i = 0; i < pos; ++i)
     {
-        cout << matr[0][i] << ' ';
+        std::cout << matr[0][i] << ' ';
     }
 }
 
 template <typename T>
-void Output(ofstream &fout, T* arr, T** matr, int32_t size, uint32_t& pos, char way)
+void Output(std::ofstream &fout, T* arr, T** matr, int32_t size, uint32_t& pos, char way)
 {
     switch (way)
     {
     case '1':
-        cout << "Array in file 'output.txt' \n";
+        std::cout << "Array in file 'output.txt' \n";
         OutputInFile(fout, arr, matr, size, pos);
         break;
     case '2':
-        cout << "Array in file 'output.txt' \n";
+        std::cout << "Array in file 'output.txt' \n";
         OutputInFile(fout, arr, matr, size, pos);
         break;
     case '3':
@@ -154,12 +149,12 @@ void InputRandom(T* arr, uint32_t size)
 }
 
 template <typename T>
-void wayInputOutput(ifstream &fin, T* arr, uint32_t size, char& way)
+void wayInputOutput(std::ifstream &fin, T* arr, uint32_t size, char& way)
 {
-    if (way == NULL)
+    if (way == ' ')
     {
-        cout << "Select the data input and output method:\n\t1.Direct input and writing to a text file\n\t2.Generating random numbers using a sensor and writing to a text filen\n\t3.Input from a text file, and output to the console.\n";
-        cin >> way;
+        std::cout << "Select the data input and output method:\n\t1.Direct input and writing to a text file\n\t2.Generating random numbers using a sensor and writing to a text filen\n\t3.Input from a text file, and output to the console.\n";
+        std::cin >> way;
     }
     switch (way) {
     case '1':
@@ -172,7 +167,7 @@ void wayInputOutput(ifstream &fin, T* arr, uint32_t size, char& way)
         InputFromFile(fin, arr, size);
         break;
     default:
-        cout << "Uncorrect valuable!!!\n";
+        std::cout << "Uncorrect valuable!!!\n";
         wayInputOutput(fin, arr, size, way);
     }
 }
@@ -202,11 +197,12 @@ void deleteArr(int32_t* intArr, double* doubleArr, char* charArr, int32_t** intM
 int32_t main()
 {
     uint32_t pos;
-    double size;
+    size_t size;
+    double checksize;
     try
     {
-        CheckSize(size);
-        char way = NULL;
+        CheckSize(checksize, size);
+        char way = ' ';
 
         int32_t* intArr = new int32_t[size];
         double* doubleArr = new double[size];
@@ -230,32 +226,32 @@ int32_t main()
             charMatr[i] = new char[size];
         }
 
-        ifstream fin("input.txt");
+        std::ifstream fin("input.txt");
         wayInputOutput(fin, intArr, size, way);
         wayInputOutput(fin, doubleArr, size, way);
         wayInputOutput(fin, charArr, size, way);
         fin.close();
 
-        cout << "\nInteger: ";
+        std::cout << "\nInteger: ";
         processArray(intArr, size);
-        cout << "\nDouble: ";
+        std::cout << "\nDouble: ";
         processArray(doubleArr, size);
-        cout << "\nChar: ";
+        std::cout << "\nChar: ";
         processArray(charArr, size);
 
-        ofstream fout("output.txt");
-        cout << "\nInteger: \n";
+        std::ofstream fout("output.txt");
+        std::cout << "\nInteger: \n";
         Output(fout, intArr, intMatr, size, pos, way);
-        cout << "\nDouble: \n";
+        std::cout << "\nDouble: \n";
         Output(fout, doubleArr, doubleMatr, size, pos, way);
-        cout << "\nChar: \n";
+        std::cout << "\nChar: \n";
         Output(fout, charArr, charMatr, size, pos, way);
         deleteArr(intArr, doubleArr, charArr, intMatr, doubleMatr, charMatr);
         fout.close();
     }
     catch (const char* msg)
     {
-        cout << msg;
+        std::cout << msg;
     }
 
 
