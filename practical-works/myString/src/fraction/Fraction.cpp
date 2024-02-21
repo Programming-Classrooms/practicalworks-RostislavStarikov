d12bced7c2cdabbbcc3b38eb8a708a23197663ef
RostislavStarikov <starikovrostislav7@gmail.com>
2024-02-21 20:13:41 +0300

    Добавил класс String

---
+++ practical-works/myString/src/fraction/Fraction.cpp
@@ -0,0 +1,180 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Fraction.hpp"
+
+String::String()
+{
+	str = 0;
+	len = 0;
+}
+
+String::String(const char* ptr)
+{
+	len = strlen(ptr);
+	str = new char[len + 1];
+	strcpy(str, ptr);
+}
+
+String::String(const String& arr) 
+{
+	len = strlen(arr.str);
+	str = new char[len + 1];
+	strcpy(str, arr.str);
+}
+
+String& String::operator = (String& arr)
+{
+	len = arr.len;
+	delete[] str;
+	str = new char[len + 1];
+	strcpy(str, arr.str);
+	return (*this);
+}
+
+String& String::operator += (const String& arr)
+{
+	size_t newLen = len + arr.len;
+	char* newStr = new char[newLen + 1];
+	strcpy(newStr, str);
+	strcat(newStr, arr.str);
+	delete[] str;
+	str = newStr;
+	len = newLen;
+	return *this;
+}
+
+String& String::operator + (const String& arr)
+{
+	size_t newLen = len + arr.len;
+	char* newStr = new char[newLen + 1];
+	strcpy(newStr, str);
+	strcat(newStr, arr.str);
+	delete[] str;
+	str = newStr;
+	len = newLen;
+	return *this;
+}
+
+bool String::operator == (const String& arr) 
+{
+	if (strlen(str) == strlen(arr.str))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool String::operator < (const String& arr)
+{
+	{
+		if (strlen(arr.str) < strlen(str))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+bool String::operator > (const String& arr)
+{
+	if (strlen(arr.str) > strlen(str))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}bool String::operator <= (const String& arr)
+{
+	{
+		if (strlen(arr.str) <= strlen(str))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+bool String::operator >= (const String& arr)
+{
+	if (strlen(arr.str) >= strlen(str))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool String::operator != (const String& arr) 
+{
+	return !(operator == (arr));
+}
+
+bool String::is_empty()
+{
+	return str == 0 || str[0] == '\0';
+}
+
+const char* String::getStr()  
+{
+	return str;
+}
+
+size_t String::getLength() 
+{
+	return len;
+}
+
+int32_t search_first_of(const char a, const String& s)
+{
+	size_t size = strlen(s.str);
+	for (size_t i = 0; i < size; ++i)
+	{
+		if (s.str[i] == a)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int32_t search_first_of(const String& find, const String& s)
+{
+	size_t size = strlen(s.str);
+	size_t sizeFind = strlen(find.str);
+	for (size_t i = 0; i < sizeFind; ++i)
+	{
+		for (size_t j = 0; j < size; ++j)
+		{
+			if (s.str[j] == find.str[i])
+			{
+				return j;
+			}
+		}
+	}
+	return -1;
+}
+
+std::ostream& operator << (std::ostream& out, const String& s)
+{
+	out << s.str;
+	return out;
+}
+
+String::~String()
+{
+	len = 0;
+	delete[] str;
+	std::cout << "\nDestruct";
+}
---
+++ practical-works/myString/src/fraction/Fraction.hpp
@@ -0,0 +1,43 @@
+#ifndef FRACTION_HPP
+#define FRACTION_HPP
+#include <iostream>
+#include <cstring>
+
+class String
+{
+private:
+	size_t len;
+	char* str;
+public:
+
+	String();
+	String(const char*);
+	String(const String&);
+
+	String& operator = (String&);
+
+	String& operator += (const String&);
+
+	String& operator + (const String&);
+
+	bool operator == (const String&) ;
+	bool operator > (const String&) ;
+	bool operator >= (const String&) ;
+	bool operator < (const String&) ;
+	bool operator <= (const String&) ;
+	bool operator != (const String&) ;
+
+	bool is_empty();
+
+	const char* getStr() ;
+
+	size_t getLength() ;
+
+	friend int32_t search_first_of(const char, const String&);
+	friend int32_t search_first_of(const String&, const String&);
+	
+	friend std::ostream& operator << (std::ostream&, const String&);
+
+	~String();
+};
+#endif
