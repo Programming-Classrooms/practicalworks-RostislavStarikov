ff751f1690c11489de088d194152ba7612c2697d
Rostislav Starikov <starikovrostislav7@gmail.com>
2024-05-26 01:27:40 +0300

    Добавил тесты

--- laboratory-task-14-2/src/tests/StudentTests/StudentTests.cpp
+++ laboratory-task-14-2/src/tests/StudentTests/StudentTests.cpp
@@ -2,3 +2,19 @@
 #include <gtest/gtest.h>
 #include "src/Student/Student.hpp"
 
+TEST(CONSTRUCTOR_TEST, DEFAULT_CONSTRUCTOR)
+{
+    Student obj;
+
+    EXPECT_EQ(obj.getCourse(), 1);
+    EXPECT_EQ(obj.getGroup(), 1);
+}
+
+TEST(CONSTRUCTOR_TEST,CONSTRUCTOR_BY_PARAMETERS)
+{
+    Student obj("Rostislav",1,7121);
+
+    EXPECT_STREQ(obj.getFullName(), "Rostislav");
+    EXPECT_EQ(obj.getCourse(), 1);
+    EXPECT_EQ(obj.getGroup(), 7121);
+}
